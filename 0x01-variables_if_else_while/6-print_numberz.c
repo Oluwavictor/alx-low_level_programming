ea122414882ea792305049a9304ed051a97bc320
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-09 14:04:41 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - print all single digit numbers of base 10
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = '0';
+	
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+		
