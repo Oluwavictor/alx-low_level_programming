140721bf3d2ec54fbf705c1b9b7b253413fa77f6
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-09 14:38:04 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet reversed using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while(alpha >= 0)
+	{
+		putchar(alpha);
+		alpha--;
+	
+	}
+
+	putchar('\n);
+
+	return (0)
+
+}
+
