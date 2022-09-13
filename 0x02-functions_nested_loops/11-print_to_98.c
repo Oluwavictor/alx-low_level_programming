8dc0b1c51109c4832813a2d31c28a80e27470123
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-13 14:44:04 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
+ */
+
+void print_to_98(int i)
+{
+	while (i < 98)
+	{
+		printf("%d" , i);
+		i++;
+	}
+
+	while (i > 98)
+	{
+		printf("%d, ", i);
+		i--;
+	}
+
+	if(i == 98)
+	{
+		printf("%d, ", i);
+
+	}
+	printf("\i");
+}
+
+
