2e46271fa15a164afd34f54e1002bfde578bbb04
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-13 14:28:37 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @p: two integer arguments
+ * @q: two integer arguments
+ * Return: sum
+ */
+
+int add(int p, int q)
+{
+	return (p + q);
+}
