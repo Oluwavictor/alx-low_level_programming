2424652d09036af0d78aabaa7bb217dc3484bf97
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-14 00:52:54 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,13 @@ void print_to_98(int i)
 {
 	if (i < 98)
 	{
-		for (i = i; n < 98; i++)
+		for (i = i; i < 98; i++)
 			printf("%d, ", i);
 		printf("%d\n", 98);
 	}
 	else
 	{
-		for (i = i; n > 98; i--)
+		for (i = i; i > 98; i--)
 			printf("%d, ", i);
 		printf("%d\n", 98);
 	}
