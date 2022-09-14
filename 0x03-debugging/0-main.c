1d066e50edf47be8c33ad34a5b5553741052ca09
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-14 09:38:11 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that prints if n is positive or negative
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
