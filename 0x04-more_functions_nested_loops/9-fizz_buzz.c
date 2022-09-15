a1250b074a07789091aa79b9a3bf084bae740bd1
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-15 12:34:08 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,9 +15,13 @@ int main(void)
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			if (i % 3 == 0)
+			{
 				printf("Fizz");
+			}
 			if (i % 5 == 0)
+			{
 				printf("Buzz");
+			}
 		}
 		else
 		{
@@ -29,3 +33,4 @@ int main(void)
 
 	printf("\n");
 	return (0);
+}
