a7b9ad3c43f00ab39ace7159e2d05224433af7df
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-21 21:31:41 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,15 +2,14 @@
 
 /**
  * cap_string - capitalize first letter of each word
- * @str: the string
+ * @s: the string
  *
  * Return: string
  */
 
 char *cap_string(char *s)
 {
-	int i = 0;
-	int j;
+	int i = 0, j;
 	char a[] = " \t\n,;.!?\"(){}";
 
 	while (*(s + i))
