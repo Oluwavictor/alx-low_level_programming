3c15e1c390110f725ad45ca287e8d4b5a75c3283
Oluwavictor <oluwavictor2009@gmail.com>
2022-09-26 18:20:52 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-	int j;
-	unsigned int result = 0;
+	unsigned int a = 0, b, t = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (accept[a])
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		b = 0;
+
+		while (s[b] != 32)
 		{
-			if (s[i] == accept[j])
+			if (accept[a] == s[b])
 			{
-				result++;
-				break;
+				t++;
 			}
+			b++;
 		}
-		if (s[j] == '\0')
-			return (result);
+		a++;
 	}
-	return (result);
+
+	return (t);
 }
