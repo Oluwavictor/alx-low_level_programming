a1fdf4a2b76a81fce50bdbc9aae4f6b3efb3697b
Oluwavictor <oluwavictor2009@gmail.com>
2022-10-04 09:45:46 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,86 +1,103 @@
 #include "main.h"
 
 /**
- * aid - helps function
- * @word: wordCount
- * @size: length
- * @s : array you want to assign
- * @str : string
+ * countWords - counts the numbers o words
+ * @str: source string
+ * Return: the number of words.
  */
 
-char **aid(int word, int size, char *str, char **s)
+int countWords(char *str)
 {
-	int i, j, k;
+	int words;
+	words = 0;
 
-	j = 0;
-	for (i = 0; i < word; i++)
+	while (*str)
 	{
-		k = 0;
-		for (; j < size; j++)
+		if (*str != ' ')
 		{
-			if (str[0] != ' ' || str[j] != ' ')
-			{
-				s[i][k] = str[j];
-				k++;
-			}
-			if (j != 0 && str[j] == ' ' && str[j - 1] != ' ')
-			{
-				j++;
-				break;
-			}
+			if (*(str + 1) == ' ' || *(str + 1) == '\0')
+				words++;
+			str++;
 		}
-		s[i][k + 1] = '\0';
+		else
+			str++
 	}
-	s[word + 1] = NULL;
-	return (s);
+	return (words);
 }
 
 /**
- * strtow - splits a string into words
- * @str: input string to check
- * Return: pointer to new string
+ * countChars - counts the numbers o characters
+ * @str: source string
+ *
+ * Return: the number of words.
+ */
+
+int countChars(char *str)
+{
+	int chars;
+	
+	chars = 0;
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			if (*(str + 1) != ' ' || *(str + 1) != '\0')
+				chars++;
+			str++;
+		}
+		else
+			break;
+	}
+	return (chars);
+}
+
+/**
+ * strtow - separates the character to an string of words
+ * @str: source
+ * Return: the pointer to the new array of words.
  */
 
 char **strtow(char *str)
 {
-	int len, i, j, size, k, word;
-	char **s;
+	int numWords, numChars, i, j;
+	char **words;
 
-	if (str == NULL)
+	i = 0;
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	len = 0;
-	word = 0;
-	while (str[len] != '\0')
-	{
-		if (str[0] != ' ')
-			word++;
-		if (str[len] != ' ' && str[len - 1] == ' ' && len != 0)
-			word++;
-		len++;
-	}
-	s = (char **)malloc(sizeof(char *) * word + 1);
-	if (s == NULL)
+	numWords = countWords(str);
+	if (numWords == 0)
 		return (NULL);
-	j = 0;
-	for (i = 0; i < word; i++)
+	words = (char **) malloc((numWords + 1) * sizeof(char *));
+	if (words == NULL)
 	{
-		size = 0;
-		for (; j < len; j++)
+		free(words);
+		return (NULL);
+	}
+	while (i < numWords)
+	{
+		if (*str == ' ')
+			str++;
+		else
 		{
-			if (str[0] != ' ' || str[j] != ' ')
-				size++;
-			if (str[j] == ' ' && size > 0)
-				break;
-		}
-		printf("%d\n", size);
-		s[i] = (char *)malloc(sizeof(char) * size + 1);
-		if (s[i] == NULL)
-		{
-			for (k = i - 1; k >= 0; k--)
-				free(s[k]);
-			free(s);
+			numChars = countChars(str);
+			*(words + i) = (char *) malloc((numChars + 1) * sizeof(char));
+			if (*(words + i) == NULL)
+			{
+				for (j = 0; j < i; j++)
+					free(*(words + j));
+				free(words);
+				return (NULL);
+			}
+			for (j = 0; j < numChars; j++)
+			{
+				*(*(words + i) + j) = *str;
+				str++;
+			}
+			*(*(words + i) + j) = '\0';
+			i++
 		}
 	}
-	s = aid(word, len, str, s);
-	return (s);
+	*(words + numWords) = NULL;
+	return (words);
 }
