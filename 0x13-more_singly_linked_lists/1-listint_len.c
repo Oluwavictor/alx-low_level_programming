248a07ad2c64e5aedefe4f2211bc24210c6d97ac
Oluwavictor <oluwavictor2009@gmail.com>
2022-10-25 01:07:27 +0100

    inital commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h: pointer to the head of the listint_t list
+ * Return: the number of nodes
+ */
+ size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function that returns the nth node of a listint_t
+ * @head: pointer to head in function
+ * @index: index of node to grab
+ * Return: node at index
+ */
+
+ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+
+	return (head);
+}
